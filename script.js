c9b43c9c89262c185bda4c25a809e39cc43c6fa9
khja240624 <khja240624@gmail.com>
2024-08-27 19:01:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const addressList = {};
 addressList["Google Drive"] = "https://drive.google.com/drive/u/0/folders/1AiOacBgHuUoHDMmF8YirNNJh0WCXRYxE";
 addressList["Github"] = "https://github.com/khja240624";
 addressList["Zoom"] = "https://zoom.us/j/7652500624?pwd=dndW4u3ViOzaV8482AiT3X7flowHXl.1";
-addressList["CodeTogether(Eclipse)"] = "https://live.codetogether.io/#/PFoAPW8OEANdR75RFcDcmW/aqKkk3afRre8hkFzhQUcY8";
+addressList["KH 정보교육원"] = "https://kh-academy.co.kr/";
+addressList["CodeTogether(Eclipse)"] = "";
 addressList["CodeTogether(VSCode)"] = "";
 addressList["CodeTogether(DBeaver)"] = "";
 
@@ -16,7 +17,7 @@ btns.forEach(btn => {
 
     const url = addressList[title];
     
-    if(url === undefined || url === ""){
+    if(url === undefined || url === "") {
       alert("등록된 주소가 없습니다");
       return;
     }
