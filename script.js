8ef6f1f3f65ae816e7e592bc1de30f1dcad5825d
khja240624 <khja240624@gmail.com>
2024-09-05 09:04:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ addressList["Google Drive"] = "https://drive.google.com/drive/u/0/folders/1AiOac
 addressList["Github"] = "https://github.com/khja240624";
 addressList["Zoom"] = "https://zoom.us/j/7652500624?pwd=dndW4u3ViOzaV8482AiT3X7flowHXl.1";
 addressList["KH 정보교육원"] = "https://kh-academy.co.kr/";
-addressList["CodeTogether(Eclipse)"] = "https://live.codetogether.io/#/xyCt8IX0McFbf1d6H6SkDx/Ibzf0fGi2v6JVfMXG2iAUK";
-addressList["CodeTogether(VSCode)"] = "https://live.codetogether.io/#/fde26dab-f124-4297-a832-b2be8f4f9704/O5bORqRtTDCHbDAIH2PYIH";
+addressList["CodeTogether(Eclipse)"] = "https://live.codetogether.io/#/xyCt8IX0McFbf1d6H6SkDx/iZvQHo0rjbqPfoeIJst8Hm";
+addressList["CodeTogether(VSCode)"] = "https://live.codetogether.io/#/fde26dab-f124-4297-a832-b2be8f4f9704/Kg5Fpkd1fA7D0zvUbwM4PN";
 addressList["CodeTogether(DBeaver)"] = "";
 
 const btns = document.querySelectorAll('a.button');
