569630232aca505cf8291e01d2ad168fc71d2882
khja240624 <khja240624@gmail.com>
2024-10-10 08:59:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ addressList["Google Drive"] = "https://drive.google.com/drive/u/0/folders/1AiOac
 addressList["Github"] = "https://github.com/khja240624";
 addressList["Zoom"] = "https://zoom.us/j/7652500624?pwd=dndW4u3ViOzaV8482AiT3X7flowHXl.1";
 addressList["KH 정보교육원"] = "https://kh-academy.co.kr/";
-addressList["CodeTogether(Eclipse)"] = "https://live.codetogether.io/#/xyCt8IX0McFbf1d6H6SkDx/kZfkf4x6drn9MEMbzP9e2b";
-addressList["CodeTogether(VSCode)"]  = "https://live.codetogether.io/#/fde26dab-f124-4297-a832-b2be8f4f9704/EsMiYYHMJADzVISusYMe6n";
+addressList["CodeTogether(Eclipse)"] = "https://live.codetogether.io/#/xyCt8IX0McFbf1d6H6SkDx/821qkPDINGUjeeIOlW5Fcv";
+addressList["CodeTogether(VSCode)"]  = "https://live.codetogether.io/#/fde26dab-f124-4297-a832-b2be8f4f9704/eVv8fTdVvISpXXJRO2L7Z8";
 addressList["ERD CLOUD"] = "https://www.erdcloud.com/d/EZYqkPxgWt249r5MA";
 
 const btns = document.querySelectorAll('a.button');
