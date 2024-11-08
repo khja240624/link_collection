46ea4951cd6b3d016ad81d95bd36e005483b4097
khja240624 <khja240624@gmail.com>
2024-11-08 12:32:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ addressList["Google Drive"] = "https://drive.google.com/drive/u/0/folders/1AiOac
 addressList["Github"] = "https://github.com/khja240624";
 addressList["Zoom"] = "https://zoom.us/j/7652500624?pwd=dndW4u3ViOzaV8482AiT3X7flowHXl.1";
 addressList["KH 정보교육원"] = "https://kh-academy.co.kr/";
-addressList["CodeTogether(Eclipse)"] = "";
-addressList["CodeTogether(VSCode)"]  = "https://live.codetogether.io/#/91c79b45-5a14-4de3-a0a4-760b838f0350/MxGsxuUu3Oipu3J5vAPJUI";
+addressList["CodeTogether(Eclipse)"] = "https://live.codetogether.io/#/xyCt8IX0McFbf1d6H6SkDx/yhiOSjAQLKSEUNIcRcTOYi";
+addressList["CodeTogether(VSCode)"]  = "";
 addressList["ERD CLOUD"] = "https://www.erdcloud.com/d/EZYqkPxgWt249r5MA";
 
 const btns = document.querySelectorAll('a.button');
